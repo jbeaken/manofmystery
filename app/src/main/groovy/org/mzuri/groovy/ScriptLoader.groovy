faa21d70ed510882b0e1b81ea320b4ace291db54
jbeaken <j.beaken@elsevier.com>
2022-03-12 10:16:11 +0000

    Using file resource

--- app/src/main/groovy/org/mzuri/groovy/ScriptLoader.groovy
+++ app/src/main/groovy/org/mzuri/groovy/ScriptLoader.groovy
@@ -1,4 +1,28 @@
 package org.mzuri.groovy
 
 class ScriptLoader {
+
+    static void main(String[] args) {
+
+        def scriptLoader  = new ScriptLoader()
+
+        scriptLoader.loadScript()
+    }
+
+    def loadScript() {
+        def binding = new Binding()
+
+        File context = new File(getClass().getResource("/scripts/WelcomeScript.groovy").toURI())
+
+        def engine = new GroovyScriptEngine([context.toURI().toURL()] as URL[])
+//        def engine = new GroovyScriptEngine( ScriptLoader.getClass().getResource("resources/scripts/").toURI() as URI[])
+
+        while (true) {
+            def greeter = engine.run('WelcomeScript.groovy', binding)
+            println greeter.sayHello()
+            Thread.sleep(1000)
+        }
+
+
+    }
 }
